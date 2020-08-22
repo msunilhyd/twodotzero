ed6fdd49111a89c1918587ad6a8a9bdcbaa9a189
msunilhyd <msunilhyd@gmail.com>
2020-08-22 19:01:01 +0530

    22-Aug-20: Fri :- Spring Boot Maven Bootstrap Code

---
+++ spring-boot/Bootstrapping/bootstrapMaven/src/main/java/com/example/bootstrapMaven/BootstrapMavenApplication.java
@@ -0,0 +1,21 @@
+package com.example.bootstrapMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BootstrapMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapMavenApplication.class, args);
+	}
+	
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello world";
+	}
+
+}
---
+++ spring-boot/Bootstrapping/bootstrapMaven/src/test/java/com/example/bootstrapMaven/BootstrapMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootstrapMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
