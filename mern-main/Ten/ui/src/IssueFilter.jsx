8b85db76bfaf8908d13ec69b329e2efd2c9c2b72
msunilhyd <msunilhyd@gmail.com>
2020-09-30 10:48:20 +0530

    30-Sep-20: Wed :- Added ten chapter complete

---
+++ mern-main/Ten/ui/src/IssueFilter.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import URLSearchParams from 'url-search-params';
+import { withRouter } from 'react-router-dom';
+
+class IssueFilter extends React.Component {
+  constructor() {
+    super();
+    this.onChangeStatus = this.onChangeStatus.bind(this);
+  }
+
+  onChangeStatus(e) {
+    const status = e.target.value;
+    const { history } = this.props;
+    history.push({
+      pathname: '/issues',
+      search: status ? `?status=${status}` : '',
+    });
+  }
+
+  render() {
+    const { location: { search } } = this.props;
+    const params = new URLSearchParams(search);
+    return (
+      <div>
+        Status:
+        {' '}
+        <select value={params.get('status') || ''} onChange={this.onChangeStatus}>
+          <option value="">(All) </option>
+          <option value="New">New</option>
+          <option value="Assigned">Assigned</option>
+          <option value="Fixed">Fixed</option>
+          <option value="Closed">Closed</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default withRouter(IssueFilter);
