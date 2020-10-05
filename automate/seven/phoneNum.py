2d30a1736ac5ebd4900d9812d96b2dbb409ef528
msunilhyd <msunilhyd@gmail.com>
2020-10-05 17:45:26 +0530

    05-Oct-20: Mon :- TextInput of Chapter 12 added

--- automate/seven/phoneNum.py
+++ automate/seven/phoneNum.py
@@ -34,19 +34,33 @@
 #
 # phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 # mo = phoneNumRegex.search('My number is 415-555-4242')
-# print('Phone number found: ' + mo.group())
-
-import re
-phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
-mo = phoneNumRegex.search('My number is 415-555-4242.')
+# # print('Phone number found: ' + mo.group())
 #
+# import re
+# phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+# mo = phoneNumRegex.search('My number is 415-555-4242.')
+# #
+# # print(mo.group(1))
+# # print(mo.group(2))
+# # print(mo.group(0))
+# # print(mo.group())
+#
+# # print(mo.groups())
+#
+# areaCode, mainNumber = mo.groups()
+# print(areaCode)
+# print(mainNumber)
+
+
+#
+# import re
+#
+# phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)')
+# mo = phoneNumRegex.search('My phone number is (415) 555-4242.')
 # print(mo.group(1))
 # print(mo.group(2))
-# print(mo.group(0))
-# print(mo.group())
 
-# print(mo.groups())
 
-areaCode, mainNumber = mo.groups()
-print(areaCode)
-print(mainNumber)
+import re
+
+re.compile(r'(\(Parenthesis\)')
